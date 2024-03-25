b2c3846f7be60069cd6908d7c4a9cb98bdb3bb8a
Ethan <q40131125@gmail.com>
2024-03-25 15:30:43 +0800

    Initial Commit

---
+++ DragobBallColor_swiftUI/DragobBallColor_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragobBallColor_swiftUIApp.swift
+//  DragobBallColor_swiftUI
+//
+//  Created by Ethan Hung on 2024/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DragobBallColor_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
